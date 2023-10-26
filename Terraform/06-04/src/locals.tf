c3b0a1d7bf28e2123bde921a47aefc04dd51638d
k1235 <kanat.temp@gmail.com>
2023-10-26 10:18:11 +0200

    fin

---
+++ Terraform/06-04/src/locals.tf
@@ -0,0 +1,6 @@
+locals {
+
+  #serial-port-enable = 1
+  ssh_keys = file("~/.ssh/yan_ssh_key.pub")
+
+}
---
+++ Terraform/06-04/src1/vpc/variables.tf
@@ -0,0 +1,8 @@
+variable "subnets" {
+  type = list(object({ zone = string, cidr = list(string) }))
+}
+
+variable "env_name" {
+  type    = string
+  default = "develop"
+}
